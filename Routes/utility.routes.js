94ef40c8a53fc2d4321c1562206f109aab7f0958
Rohit Kumar <rk6093720@gmail.com>
2023-10-17 17:31:47 +0530

    utility done

--- Routes/utility.routes.js
+++ Routes/utility.routes.js
@@ -1,11 +1,15 @@
 const {Router} = require("express");
-const { getUtility, postUtility } = require("../controller/utility.controller");
+const { getUtility, postUtility, putUtility, deleteUtility } = require("../controller/utility.controller");
 
 const utilityRouter = Router();
 
 utilityRouter.get("/read",getUtility);
 
 utilityRouter.post("/create",postUtility);
+
+utilityRouter.put("/update/:id",putUtility);
+
+utilityRouter.delete("/remove/:id",deleteUtility)
 
 module.exports={
     utilityRouter
