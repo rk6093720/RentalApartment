0ad60b274cfed68ad97d27fd9e3c442058747359
Rohit Kumar <rk6093720@gmail.com>
2023-10-26 10:52:16 +0530

    resolving

--- controller/landLord.controller.js
+++ controller/landLord.controller.js
@@ -156,7 +156,7 @@ const deleteLandlord =async(req,res)=>{
 // Multer configuration for file uploads
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "images");
+        cb(null, "images/");
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = uuidv4() + path.extname(file.originalname);
